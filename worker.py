c92cd62bd187bc4ec5e316461c8495051ae11c81
Luigi Di Fraia <luigidifraia@gmail.com>
2020-03-02 17:47:49 +0000

    A few rationalizations

--- worker.py
+++ worker.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python
 
+#################
+# Data uploader #
+#################
+
+def upload_data(ds, type, bands, **kwargs):
+
+    from export import s3_export_xarray_to_geotiff
+
+    bucket = 'public-eo-data'
+    prefix = 'luigi'
+
+    product = kwargs.get('product')
+    pn = product[0:3] if product.startswith('ls') else product[0:2]
+    tf = kwargs.get('time_from')
+    tt = kwargs.get('time_to')
+    xf = kwargs.get('longitude_from')
+    xt = kwargs.get('longitude_to')
+    yf = kwargs.get('latitude_from')
+    yt = kwargs.get('latitude_to')
+
+    for band in bands:
+        s3_export_xarray_to_geotiff(ds, band, bucket, f"{prefix}/{pn}_{type}_{tf}_{tt}_epsg3460_{xf}_{xt}_{yf}_{yt}_{band}.tif")
+
+
 ###################
 # Request handler #
 ###################
 
-def process_request(dc, client, type, product, **kwargs):
-    bucket = 'public-eo-data'
-    prefix = 'luigi'
+def process_request(dc, client, type, **kwargs):
 
     try:
         if type == "geomedian":
             from geomedian import process_geomedian
 
-            ds = process_geomedian(dc=dc, client=client, product=product, **kwargs)
+            ds = process_geomedian(dc=dc, client=client, **kwargs)
+            save_bands = ['red', 'green', 'blue', 'nir', 'swir1', 'swir2']
 
-            if ds:
-                from export import s3_export_xarray_to_geotiff
-
-                save_bands = ['red', 'green', 'blue', 'nir', 'swir1', 'swir2']
-                for band in save_bands:
-                    s3_export_xarray_to_geotiff(ds, band, bucket, f"{prefix}/{product}-routine-geomedian-{band}.tif")
+        if ds:
+            upload_data(ds=ds, type=type, bands=save_bands, **kwargs)
 
     except Exception as e:
         print("Error: " + str(e))
 
     finally:
         client.restart()
+
 
 ######################
 # Product generation #
@@ -34,8 +54,10 @@ def process_request(dc, client, type, product, **kwargs):
 import json
 
 def process_job(dc, client, json_data):
+
     loaded_json = json.loads(json_data)
     process_request(dc, client, **loaded_json)
+
 
 ##################
 # Job processing #
