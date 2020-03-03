e5811a82cfd4c1a66e36666b1dbcd1ee31478bd5
Luigi Di Fraia <luigidifraia@gmail.com>
2020-03-03 10:50:48 +0000

    Simplify data upload logic

--- worker.py
+++ worker.py
@@ -1,33 +1,14 @@
 #!/usr/bin/env python
 
-###############
-# S3 uploader #
-###############
-
-def s3_upload_band_file(ds, band, no_data, output_crs, bucket, destination):
-    from os.path import basename
-    from export import export_xarray_to_geotiff
-    from s3 import s3_upload_file
-
-    fname = basename(destination)
-
-    export_xarray_to_geotiff(ds, fname, bands=[band], no_data=no_data, crs=output_crs, x_coord='x', y_coord='y')
-
-    try:
-        s3_upload_file(fname, bucket, destination);
-
-    except Exception as e:
-        print("Error: " + str(e))
-
-    finally:
-        os.remove(fname)
-
-
 #################
 # Data uploader #
 #################
 
 def save_data(ds, type, bands, product, time_from, time_to, output_crs, bucket='public-eo-data', prefix='luigi', **kwargs):
+    from os.path import basename
+    from export import export_xarray_to_geotiff
+    from s3 import s3_upload_file
+
     pn = product[0:3] if product.startswith('ls') else product[0:2]
     no_data = -9999 if product.startswith('ls') else 0
 
@@ -39,12 +20,19 @@ def save_data(ds, type, bands, product, time_from, time_to, output_crs, bucket='
     crs = output_crs.lower().replace(':', '')
 
     for band in bands:
-        s3_upload_band_file(ds,
-                            band,
-                            no_data,
-                            output_crs,
-                            bucket,
-                            f"{prefix}/{pn}_{type}_{time_from}_{time_to}_{crs}_{x_from}_{y_from}_{x_to}_{y_to}_{band}.tif")
+        destination = f"{prefix}/{pn}_{type}_{time_from}_{time_to}_{crs}_{x_from}_{y_from}_{x_to}_{y_to}_{band}.tif"
+        fname = basename(destination)
+
+        export_xarray_to_geotiff(ds, fname, bands=[band], no_data=no_data, crs=output_crs, x_coord='x', y_coord='y')
+
+        try:
+            s3_upload_file(fname, bucket, destination);
+
+        except Exception as e:
+            print("Error: " + str(e))
+
+        finally:
+            os.remove(fname)
 
 
 ###################
