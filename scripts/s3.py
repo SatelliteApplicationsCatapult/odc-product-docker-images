1c8c11c1513db44c497642b194daacdc9183b48f
Luigi Di Fraia <luigidifraia@gmail.com>
2020-03-10 09:01:59 +0000

    Convert to a simple client class

--- scripts/s3.py
+++ scripts/s3.py
@@ -1,16 +1,21 @@
 import os
 import boto3
 
-def s3_upload_file(source, bucket, destination):
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID")
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
-    aws_s3_endpoint=os.getenv("AWS_S3_ENDPOINT")
+class S3Client:
+    """
+    A simple interface for uploading small files to an S3 bucket
+    """
 
-    endpoint_url=f"http://{aws_s3_endpoint}"
+    def __init__(self):
+        aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+        aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+        aws_s3_endpoint_url = os.getenv("AWS_S3_ENDPOINT_URL")
 
-    s3 = boto3.client('s3',
-                      aws_access_key_id=aws_access_key_id,
-                      aws_secret_access_key=aws_secret_access_key,
-                      endpoint_url=endpoint_url)
+        self.s3_client = boto3.client('s3',
+                                      aws_access_key_id=aws_access_key_id,
+                                      aws_secret_access_key=aws_secret_access_key,
+                                      endpoint_url=aws_s3_endpoint_url)
 
-    s3.upload_file(source, bucket, destination)
+    def upload_file(self, source, bucket, destination):
+        self.s3_client.upload_file(source, bucket, destination)
+
