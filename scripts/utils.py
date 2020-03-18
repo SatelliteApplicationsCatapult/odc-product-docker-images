ba5c8957ca86f7feaf02e0684d48df8793f85e76
Luigi Di Fraia <luigidifraia@gmail.com>
2020-03-18 15:54:20 +0000

    Make code CRS independent as long as it's not EPSG:4326 and its unit is meter

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,11 +10,11 @@ import yaml
 # Utils #
 #########
 
-def get_ds_extents(ds, x_coord='longitude', y_coord='latitude'):
-    x_from = float(ds[x_coord][0])
-    x_to = float(ds[x_coord][-1])
-    y_from = float(ds[y_coord][0])
-    y_to = float(ds[y_coord][-1])
+def get_ds_extents(ds):
+    x_from = float(ds['x'][0])
+    x_to = float(ds['x'][-1])
+    y_from = float(ds['y'][0])
+    y_to = float(ds['y'][-1])
 
     return x_from, x_to, y_from, y_to
 
@@ -51,16 +51,12 @@ def save_data(s3_client,
     no_data = -9999 if product.startswith('ls') else 0
 
     # Get dataset extents
-    if output_crs == 'EPSG:4326':
-        x_from, x_to, y_from, y_to = get_ds_extents(ds)
-    else:
-        x_from, x_to, y_from, y_to = get_ds_extents(ds, x_coord='x', y_coord='y')
+    x_from, x_to, y_from, y_to = get_ds_extents(ds)
 
     # Generate EPSG:4326 extents for band filename upon request
     if epsg4326_naming == 'True':
-        if output_crs != 'EPSG:4326':
-            x_from, y_from = point_to_epsg4326(output_crs, x_from, y_from)
-            x_to, y_to = point_to_epsg4326(output_crs, x_to, y_to)
+        x_from, y_from = point_to_epsg4326(output_crs, x_from, y_from)
+        x_to, y_to = point_to_epsg4326(output_crs, x_to, y_to)
 
     crs = output_crs.lower().replace(':', '')
 
@@ -70,10 +66,7 @@ def save_data(s3_client,
         fname = basename(destination)
         logging.debug("Saving band file %s.", fname)
 
-        if output_crs == 'EPSG:4326':
-            export_xarray_to_geotiff(ds, fname, bands=[band], no_data=no_data)
-        else:
-            export_xarray_to_geotiff(ds, fname, bands=[band], no_data=no_data, crs=output_crs, x_coord='x', y_coord='y')
+        export_xarray_to_geotiff(ds, fname, bands=[band], no_data=no_data, crs=output_crs, x_coord='x', y_coord='y')
 
         if cogeo_output == 'True':
             import shlex, subprocess
@@ -118,16 +111,12 @@ def save_metadata(s3_client,
     pn = product[0:3] if product.startswith('ls') else product[0:2]
 
     # Get dataset extents
-    if output_crs == 'EPSG:4326':
-        x_from, x_to, y_from, y_to = get_ds_extents(ds)
-    else:
-        x_from, x_to, y_from, y_to = get_ds_extents(ds, x_coord='x', y_coord='y')
+    x_from, x_to, y_from, y_to = get_ds_extents(ds)
 
     # Generate EPSG:4326 extents for band filename upon request
     if epsg4326_naming == 'True':
-        if output_crs != 'EPSG:4326':
-            x_from, y_from = point_to_epsg4326(output_crs, x_from, y_from)
-            x_to, y_to = point_to_epsg4326(output_crs, x_to, y_to)
+        x_from, y_from = point_to_epsg4326(output_crs, x_from, y_from)
+        x_to, y_to = point_to_epsg4326(output_crs, x_to, y_to)
 
     crs = output_crs.lower().replace(':', '')
 
@@ -154,18 +143,11 @@ def save_metadata(s3_client,
     metadata_obj_key = f"s3://{bucket}/{destination}"
 
     # Get dataset extents
-    if output_crs == 'EPSG:4326':
-        x_from, x_to, y_from, y_to = get_ds_extents(ds)
-    else:
-        x_from, x_to, y_from, y_to = get_ds_extents(ds, x_coord='x', y_coord='y')
+    x_from, x_to, y_from, y_to = get_ds_extents(ds)
 
     # Generate EPSG:4326 extents for metadata extents
-    if output_crs == 'EPSG:4326':
-        longitude_from, latitude_from = x_from, y_from
-        longitude_to, latitude_to = x_to, y_to
-    else:
-        longitude_from, latitude_from = point_to_epsg4326(output_crs, x_from, y_from)
-        longitude_to, latitude_to = point_to_epsg4326(output_crs, x_to, y_to)
+    longitude_from, latitude_from = point_to_epsg4326(output_crs, x_from, y_from)
+    longitude_to, latitude_to = point_to_epsg4326(output_crs, x_to, y_to)
 
     if job_code == 'geomedian':
         doc = generate_datacube_metadata(metadata_obj_key,
